78e0b8fd01750703c997f588fdda213c12f5c9ab
Jonathan Hakimi <phantop@protonmail.com>
2018-09-09 12:37:43 -0400

    Faster scrolling

--- config.h
+++ config.h
@@ -197,8 +197,8 @@ static MouseShortcut mshortcuts[] = {
 
 MouseKey mkeys[] = {
 	/* button               mask            function        argument */
-	{ Button4,              XK_NO_MOD,      kscrollup,      {.i =  1} },
-	{ Button5,              XK_NO_MOD,      kscrolldown,    {.i =  1} },
+	{ Button4,              XK_NO_MOD,      kscrollup,      {.i =  3} },
+	{ Button5,              XK_NO_MOD,      kscrolldown,    {.i =  3} },
 };
 
 /* Internal keyboard shortcuts. */
